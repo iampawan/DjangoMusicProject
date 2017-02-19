6fd84d8591753895eda97041e43abe0662fae673
Pawan Kumar <pawan221b@gmail.com>
2017-02-19 10:23:44 +0530

    * Completed First Phase*

--- music/models.py
+++ music/models.py
@@ -1,18 +1,24 @@
-from __future__ import unicode_literals
-
+from django.contrib.auth.models import Permission, User
 from django.db import models
 
-# Create your models here.
+
 class Album(models.Model):
-    artist = models.CharField(max_length=200)
+    user = models.ForeignKey(User, default=1)
+    artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=1000)
+    album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.album_title +"-"+self.artist
+        return self.album_title + ' - ' + self.artist
+
 
 class Song(models.Model):
-    album = models.ForeignKey(Album,on_delete=models.CASCADE)
-    song_title = models.CharField(max_length=1000)
-    file_type = models.CharField(max_length=10)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
